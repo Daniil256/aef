3c07e83d904275490a42b92c71b7fec6cc54a361
Daniil256 <89648303+Daniil256@users.noreply.github.com>
2022-01-30 18:41:50 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const text = document.querySelector('.text')
 const display = document.querySelector('.display')
 const btn_num = document.querySelectorAll('.num')
 const btn = document.querySelectorAll('.btn')
+const func = document.querySelectorAll('.func')
 const dot = document.querySelector('.dot')
 
 const btn_plus = document.querySelector('.plus')
@@ -13,109 +14,74 @@ const btn_equal = document.querySelector('.equal')
 const btn_clear = document.querySelector('.clear')
 const btn_backspace = document.querySelector('.backspace')
 
-let ar = []
 let operator = ''
-let result = 0
-let x = ''
+let x = 0
+let y = 0
 let symbol = []
 
 let width_text_default = Number(window.getComputedStyle(text).width.replace(/[a-z]/g, ""))
-let fz = 15
+let fz = 70
+let width_text
+let width_display
+function ss() {
+    if (Number(text.innerHTML[text.innerHTML.length - 1]) == text.innerHTML[text.innerHTML.length - 1] && text.innerHTML != '0') {
+        x = Number(x)
+        if (y == 0) y = x
+        if (operator == '-') y -= x
+        if (operator == '+') y += x
+        if (operator == '*') y *= x
+        if (operator == '/') y /= x
+        if (operator != '/' && operator != '*' && operator != '+' && operator != '-' && operator != '') display.innerHTML = 'Произошла ошибка'
+        x = 0
+    }
+}
 
-
+//уменьшение размера шрифта, когда число не влазит
 document.querySelector('body').addEventListener('click', function () {
-    console.log('ar', ar);
-    console.log('result', result);
-    console.log('x', x);
-    console.log('operator', operator);
-    console.log("======================");
-    let width_text = Number(window.getComputedStyle(text).width.replace(/[a-z]/g, ""))
-    let width_display = Number(window.getComputedStyle(display).width.replace(/[a-z]/g, ""))
-    // console.log(width_display - width_text);
-    // console.log(width_display);
-    // console.log(width_text);
-    if (width_display - width_text < 30) {
-        text.style.fontSize = fz + 'vmin'
-        if (fz > 4) fz -= 1
-        width_text_default = width_text
-        return
-    }
-    if (width_text_default - width_text > 0) {
-        text.style.fontSize = fz + 'vmin'
-        if (fz < 15) fz += 1
-        return
+    console.log(' x', x, '\n', 'y', y, '\n', 'operator', operator, '\n', "======================");
+    width_text = Number(window.getComputedStyle(text).width.replace(/[a-z]/g, ""))
+    width_display = Number(window.getComputedStyle(display).width.replace(/[a-z]/g, ""))
+    // console.log('display', width_display, '\n', 'text', width_text, '\n,', '======================');
+    if (width_display < width_text) {
+        fz = (90 * fz) / 100
+        text.style.fontSize = fz + 'px'
     }
 })
+//вычитание
 btn_minus.addEventListener('click', function () {
-    if (Number(text.innerHTML[text.innerHTML.length - 1]) == text.innerHTML[text.innerHTML.length - 1]) {
-        ar.push(Number(x))
-        x = ''
-        if (operator == '-') result -= ar[ar.length - 1]
-        if (operator == '+') result += ar[ar.length - 1]
-        if (operator == '*') result *= ar[ar.length - 1]
-        if (operator == '/') result /= ar[ar.length - 1]
-        if (operator == '') result = ar[ar.length - 1]
-        text.innerHTML = result + '-'
+    ss()
+    if (Number(text.innerHTML[text.innerHTML.length - 1]) == text.innerHTML[text.innerHTML.length - 1] && text.innerHTML != '0') {
+        text.innerHTML = y + '-'
         operator = '-'
-    }
-    if (text.innerHTML.length == 0) {
-        text.innerHTML += '-'
-        x += '-'
+    } else {
+        text.innerHTML = '-'
+        x = '-'
     }
 })
+//прибавление
 btn_plus.addEventListener('click', function () {
-    if (Number(text.innerHTML[text.innerHTML.length - 1]) == text.innerHTML[text.innerHTML.length - 1]) {
-        ar.push(Number(x))
-        x = ''
-        if (operator == '-') result -= ar[ar.length - 1]
-        if (operator == '+') result += ar[ar.length - 1]
-        if (operator == '*') result *= ar[ar.length - 1]
-        if (operator == '/') result /= ar[ar.length - 1]
-        if (operator == '') result = ar[ar.length - 1]
-        text.innerHTML = result + '+'
-        operator = '+'
-    }
+    ss()
+    text.innerHTML = y + '+'
+    operator = '+'
 })
-btn_equal.addEventListener('click', function () {
-    ar.push(Number(x))
-    if (operator == '+') result += ar[ar.length - 1]
-    if (operator == '*') result *= ar[ar.length - 1]
-    if (operator == '-') result -= ar[ar.length - 1]
-    if (operator == '/') result /= ar[ar.length - 1]
-    text.innerHTML = result
-    //x = 0
-})
-
-
-
-btn_division.addEventListener('click', function () {
-    if (Number(text.innerHTML[text.innerHTML.length - 1]) == text.innerHTML[text.innerHTML.length - 1]) {
-        ar.push(Number(x))
-        x = ''
-        if (operator == '-') result -= ar[ar.length - 1]
-        if (operator == '+') result += ar[ar.length - 1]
-        if (operator == '*') result *= ar[ar.length - 1]
-        if (operator == '/') result /= ar[ar.length - 1]
-        if (operator == '') result = ar[ar.length - 1]
-        text.innerHTML = result
-        text.innerHTML += '/'
-        operator = '/'
-    }
-})
+//умножение
 btn_multiplication.addEventListener('click', function () {
-    if (Number(text.innerHTML[text.innerHTML.length - 1]) == text.innerHTML[text.innerHTML.length - 1]) {
-        ar.push(Number(x))
-        x = ''
-        if (operator == '-') result -= ar[ar.length - 1]
-        if (operator == '+') result += ar[ar.length - 1]
-        if (operator == '*') result *= ar[ar.length - 1]
-        if (operator == '/') result /= ar[ar.length - 1]
-        if (operator == '') result = ar[ar.length - 1]
-        text.innerHTML = result
-        text.innerHTML += '*'
-        operator = '*'
-    }
+    ss()
+    text.innerHTML = y + '*'
+    operator = '*'
 })
+//деление
+btn_division.addEventListener('click', function () {
+    ss()
+    text.innerHTML = y + '/'
+    operator = '/'
+})
+//равно
+btn_equal.addEventListener('click', function () {
+    ss()
+    text.innerHTML = y
+})
+//появление цифр на экрне при клике
 for (let i = 0; i < btn_num.length; i++) {
     btn_num[i].addEventListener('click', function () {
         if (text.innerHTML == '0') text.innerHTML = ''
@@ -123,21 +89,22 @@ for (let i = 0; i < btn_num.length; i++) {
         x += btn_num[i].innerHTML
     })
 }
+//точка
 dot.addEventListener('click', function () {
     if (Number(text.innerHTML[text.innerHTML.length - 1]) == text.innerHTML[text.innerHTML.length - 1]) {
         text.innerHTML += dot.innerHTML
         x += dot.innerHTML
     }
 })
+//кнопка очистить все
 btn_clear.addEventListener('click', function () {
     text.innerHTML = '0'
-    result = 0
-    ar = []
     operator = ''
-    x = ''
-    text.style.fontSize = '15vmin'
-
+    x = 0
+    y = 0
+    text.style.fontSize = '70px'
 })
+//кнопка удалить один символ
 btn_backspace.addEventListener('click', function () {
     symbol = text.innerHTML.split('')
     symbol = symbol.slice(0, symbol.length - 1)
@@ -145,17 +112,3 @@ btn_backspace.addEventListener('click', function () {
     if (text.innerHTML == '') text.innerHTML = '0'
     console.log('symbol', symbol);
 })
-
-
-//функция для решения больших выражений
-// btn_equal.addEventListener('click', function () {
-//     ar = text.innerHTML.match(/\d+/g)
-//     if (ar && ar.length > 1) {
-//         for (let i = 0; i < ar.length; i++) {
-//             result += Number(ar[i])
-//         }
-//         text.innerHTML = result
-//         console.log(ar)
-//         console.log(result)
-//     }
-// })
